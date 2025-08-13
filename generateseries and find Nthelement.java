05e27ec19eaebf3c1733c00e65d813e00ef053f8
Brahma K <kbrahma114@gmail.com>
2025-08-13 08:30:41 +0530

    commit

---
+++ generateseries and find Nthelement.java
@@ -0,0 +1,27 @@
+public class generateseries and find Nthelement {
+    public int seriesN(int input1, int input2, int input3, int input4){
+    // Read only region end
+   
+    if (input4 == 1) {
+        return input1;
+    }
+    if (input4 == 2) {
+        return input2;
+    }
+    if (input4 == 3) {
+        return input3;
+    }
+    int gap1 = input2 - input1;
+    int gap2 = input3 - input2; 
+    int currentVal = input3;
+    for (int i = 4; i <= input4; i++) {
+        if (i % 2 == 0) {
+            currentVal += gap1;
+        } else { 
+            currentVal += gap2;
+        }
+    }
+return currentVal;
+}
+    
+}
