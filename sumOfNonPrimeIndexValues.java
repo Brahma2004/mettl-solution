2e4e5a0ee844880ec4eb37f68917878b4c1df6e6
Brahma K <kbrahma114@gmail.com>
2025-08-17 07:33:20 +0530

    commit

--- sumOfNonPrimeIndexValues.java
+++ sumOfNonPrimeIndexValues.java
@@ -1,14 +1,27 @@
-public class sumOfNonPrimeIndexValues {
-   int sum=0;
+class UserMainCode
+{
+    public int sumOfNonPrimeIndexValues(int[] input1, int input2)
+    {
+        int sum = 0;
+        for (int i = 0; i < input2; i++) {
+            if (!isprime(i)) {
+                sum += input1[i];
+            }
+        }
+        return (sum);
+    }
 
-for(int i = 0; i<input2;i++){
+    private boolean isprime(int num)
+    {
+        if (num <= 1) {
+            return false;
+        }
 
-if(!isprime(i)){
-
-
-sum += input1[i];
-
-}
-
- 
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
